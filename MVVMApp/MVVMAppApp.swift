220b05f887f326208a8554c9143f31541f5ba675
tominaga.takuya@tecotec.co.jp <5bsu!8A#A9#Tee>
2022-11-15 16:32:21 +0900

    Initial Commit

---
+++ MVVMApp/MVVMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAppApp.swift
+//  MVVMApp
+//
+//  Created by 冨永 拓弥 on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
